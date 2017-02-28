53c681bad873748a82957e861b71624c48bf9b43
papaCarlo <ag.gannenko@gmail.com>
2017-02-28 13:53:35 +0200

    updatedTests

---
+++ src/test/java/mainPack/CreateTestUser.java
@@ -0,0 +1,51 @@
+package mainPack;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+public class CreateTestUser {
+    WebDriver webDriver;
+
+    @Test
+    public void createTestUser() {
+        webDriver = new ChromeDriver();
+        webDriver.manage().window().maximize();
+        webDriver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
+        webDriver.get("http://developers.facebook.com");
+        webDriver.findElement(By.xpath(".//a[contains(text(),'Log In')]")).click();
+
+        webDriver.findElement(By.xpath(".//input[@id='email']")).clear();
+        webDriver.findElement(By.xpath(".//input[@id='email']")).sendKeys("ion.aop.fbdev@ionidea.com");
+
+        webDriver.findElement(By.xpath(".//input[@id='pass']")).clear();
+        webDriver.findElement(By.xpath(".//input[@id='pass']")).sendKeys("rojoh2DooZieB3om");
+
+        webDriver.findElement(By.xpath(".//button[@id='loginbutton']")).click();
+
+        webDriver.get("https://developers.facebook.com/apps/1779467435609829/roles/test-users/");
+        webDriver.findElement(By.xpath(".//a[@ajaxify='/apps/async/test-users/create/dialog/?app_id=1779467435609829']")).click();
+
+        try {
+            Thread.sleep(1000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        webDriver.findElement(By.xpath(".//button[contains(text(),'Create Test User')]")).click();
+        //webDriver.findElement(By.xpath(".//table[@id='u_0_x']/tbody/tr[2]/td[2]")).click();
+
+
+
+
+
+
+
+    }
+}
--- src/test/java/mainPack/FirstTest.java
+++ src/test/java/mainPack/FirstTest.java
@@ -55,7 +55,5 @@ public class FirstTest {
         Assert.assertTrue(webDriver.findElement(By.xpath(".//h3")).isDisplayed());
 
         webDriver.quit();
-
     }
-
 }
